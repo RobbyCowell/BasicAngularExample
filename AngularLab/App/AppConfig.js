1ea2ff5fd745a80eee8d2359b2c90e35db59b37d
RobbyCowell <robby.cowell@gmail.com>
2014-02-18 16:21:41 +0000

    init

---
+++ AngularLab/App/AppConfig.js
@@ -0,0 +1,14 @@
+﻿demoApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/view1',
+            {
+                controller: 'SimpleController',
+                templateUrl: 'Partials/View1.html'
+            })
+        .when('/view2',
+            {
+                controller: 'SimpleController',
+                templateUrl: 'Partials/View2.html'
+            })
+        .otherwise({ redirectTo: '/view1' });
+});
---
+++ AngularLab/App/Factories.js
@@ -0,0 +1,16 @@
+﻿demoApp.factory('simpleFactory', function () {
+
+    var customers = [
+       { name: 'John Smith', city: 'San Francisco' },
+       { name: 'Jonn Doe', city: 'New York City' },
+       { name: 'Jane Doe', city: 'Miami' }
+    ];
+
+    var factory = {};
+
+    factory.getCustomers = function () {
+        return customers;
+    };
+
+    return factory;
+});
